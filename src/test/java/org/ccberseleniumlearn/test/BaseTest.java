3dfff0051274f4c8450c9d2788d3847834a8ef86
Kedar Thatte <talk.kedar@gmail.com>
2018-12-06 12:12:18 +0530

    Initial Commit

---
+++ src/test/java/org/ccberseleniumlearn/test/BaseTest.java
@@ -0,0 +1,23 @@
+package org.ccberseleniumlearn.test;
+
+import org.ccberseleniumlearn.core.BaseReusable;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BaseTest extends BaseReusable {
+
+    /*public BaseTest(WebDriver driver){
+        this.driver=driver;
+    }*/
+
+
+    @Test
+    public void checkTitle(){
+
+        //driver.getCurrentUrl();
+        Assert.assertEquals("Gmail",driver.getTitle());
+        System.out.println("Test Passed");
+    }
+
+}
